3df803956e79ab81b709f895bdabdf662d5cf32b
yokn <yokn@protonmail.com>
2020-12-10 20:57:32 +0300

    add Users_controller#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,17 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_users_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
+  end
 end
